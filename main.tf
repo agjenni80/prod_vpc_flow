6646363aa20db21c01de945d917d7785a08f2172
Amanda Jennings <38109573+agjenni80@users.noreply.github.com>
2019-11-01 17:02:04 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+//--------------------------------------------------------------------
+// Modules
+module "vpc_flowlogs" {
+  source  = "app.terraform.io/agjenni/vpc-flowlogs/aws"
+  version = "0.0.4"
+
+  bucket_name_prefix = "hc-sec-flow-logs"
+  bucket_region = "us-east-1"
+  vpc_id = "vpc-0014580f256a1c96b"
+}
