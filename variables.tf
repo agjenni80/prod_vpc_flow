8573f659af52c7471b2d486cf7e734ac629aee82
Amanda Jennings <38109573+agjenni80@users.noreply.github.com>
2019-11-01 17:08:20 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "bucket_name_prefix" {
+  description = "S3 bucket name"
+  default = "hc-sec-flow-logs"
+}
+
+variable "bucket_region" {
+  description = "S3 bucket region"
+  default = "us-east-1"
+}
+
+variable "vpc_id" {
+  description = "id of vpc"
+}
